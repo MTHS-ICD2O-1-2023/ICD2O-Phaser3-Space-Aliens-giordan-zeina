55186e21109c99c66488d45cf4184acfe37c9c45
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-05-22 18:29:10 +0000

    done

---
+++ js/game.js
@@ -0,0 +1,54 @@
+/* global Phaser */
+
+// Copyright (c) 2024 Giordan Zeina All rights reserved
+//
+// Created by: Giordan zeina
+// Created on: Apr 2024
+// This is the Phaser3 configuration file
+
+// scene import statements
+import SplashScene from "./splashScene.js"
+import TitleScene from "./titleScene.js"
+import MenuScene from "./menuScene.js"
+import GameScene from "./gameScene.js"
+
+// create thw new scenes
+const splashScene = new SplashScene()
+const titleScene = new TitleScene()
+const menuScene = new MenuScene()
+const gameScene = new GameScene()
+
+/**
+ * start phaser game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  //set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we replaced it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+};
+
+const game = new Phaser.Game(config)
+// console.log(game)
+
+// load scene
+// Note: remember any "key" is global and CAN NOT be reused!
+game.scene.add("splashScene", splashScene)
+game.scene.add("titleScene", titleScene)
+game.scene.add("menuScene", menuScene)
+game.scene.add("gameScene", gameScene)
+
+// the start scene
+game.scene.start("splashScene")
---
+++ js/gameScene.js
@@ -0,0 +1,124 @@
+/* global Phaser */
+
+// Copyright (c) 2024 Giordan Zeina All rights reserved
+//
+// Created by: Giordan Zeina
+// Created on: May 2024
+// This is the Game Scene
+
+/**
+ * This class is the Game Scene
+ */
+class GameScene extends Phaser.Scene {
+  // create an alien
+  createAlien() {
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1 // this will get a number between 1 and 1920
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1 // this will get a number between 1 and 50;
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, "alien")
+    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.x = alienXVelocity
+    this.alienGroup.add(anAlien)
+  }
+  constructor() {
+    super({ key: "gameScene" })
+
+    this.background = null
+    this.ship = null
+    this.fireMissile = false
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * This method is called by the Scene Manager when the scene starts,
+   *   before preload() and create().
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff")
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to load assets
+   */
+  preload() {
+    console.log("Game Scene")
+
+    // images
+    this.load.image("startBackground", "./assets/starBackground.png")
+    this.load.image("ship", "./assets/spaceShip.png")
+    this.load.image("missile", "assets/missile.png")
+    this.load.image("alien", "assets/alien.png")
+    // sound
+    this.load.audio("laser", "assets/laser1.wav")
+  }
+
+  /**
+   * Can be defined on your own Scenes.
+   * Use it to create your game objects
+   * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
+   */
+  create(data) {
+    this.background = this.add.image(0, 0, "startBackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+    // create a group for the missiles
+    this.missileGroup = this.physics.add.group()
+
+    // create a group for aliens
+    this.alienGroup = this.add.group()
+    this.createAlien()
+  }
+
+  /**
+   * Should be overridden by your own Scenes.
+   * This method is called once per game step while the scene is running
+   * @param {number} time - The current time.
+   * @param {number} delta - the delta time in ms since the last frame 
+   */
+  update(time, delta) {
+    const keyLeftObj = this.input.keyboard.addKey("LEFT")
+    const keyRightObj = this.input.keyboard.addKey("RIGHT")
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
+
+    if (keyLeftObj.isDown === true) {
+      this.ship.x -= 15
+      if (this.ship.x < 0) {
+        this.ship.x = 0
+      }
+    }
+
+    if (keyRightObj.isDown === true) {
+      this.ship.x += 15
+      if (this.ship.x > 1920) {
+        this.ship.x = 1920
+      }
+    }
+
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, "missile")
+        this.missileGroup.add(aNewMissile)
+        this.sound.play("laser")
+      }
+    }
+
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
+    }
+
+    this.missileGroup.children.each(function (item) {
+      item.y = item.y - 15
+      if (item.y < 0) {
+        item.destroy()
+      }
+    })
+  }
+}
+
+export default GameScene
